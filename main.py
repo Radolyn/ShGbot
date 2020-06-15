97ff71e061c878588465ce2abc8d15d64897d2ef
SharapaGorg <62620955+SharapaGorg@users.noreply.github.com>
2020-06-15 13:54:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import discord
+import json
+import requests
 from discord.ext import commands
 from config import settings
 
 bot = commands.Bot(command_prefix = settings['PREFIX'])
 
-@bot.command(pass_context=True) 
+@bot.command() 
 async def _hola_(ctx, arg):
     await ctx.send(arg)
 
@@ -13,8 +15,20 @@ async def hello(ctx):
     author = ctx.message.author
     await ctx.send(f'Hello, {author.mention}!')
 
+@bot.command()
+async def fox(ctx):
+    response = requests.get('https://some-random-api.ml/img/fox') # Get-запрос
+    json_data = json.loads(response.text) # Извлекаем JSON
+
+    embed = discord.Embed(color = 0xff9900, title = 'Random Fox') # Создание Embed'a
+    embed.set_image(url = json_data['link']) # Устанавливаем картинку Embed'a
+    await ctx.send(embed = embed) # Отправляем Embed
+
+print('Work is in full swing...')
 bot.run(settings['TOKEN'])
+print('Bot stoped.')
 
 #D✔Бот for discord channel
 #NzIxODQ2ODk5OTg0MDM5OTY5.Xuaeyg.08dfDqsAcWxBDv6wAfXxkXe_fCg'
 #https://discord.com/developers/applications/721846899984039969/information
+
