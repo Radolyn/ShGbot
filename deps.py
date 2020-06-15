d717b91350a965057764e32066890a7a1ba888bb
SharapaGorg <62620955+SharapaGorg@users.noreply.github.com>
2020-06-15 13:16:45 +0300

    Add files via upload

---
+++ deps.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+def run_python_tool(command):
+    os.system(
+        'cd \"' + os.path.dirname(sys.executable) + '\" && ' + os.path.basename(sys.executable) + ' -m ' + command)
+
+run_python_tool('pip install --upgrade pip')
+
+run_python_tool('pip install discord --user')
+
+print('Bot ready to work.')
