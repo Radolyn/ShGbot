d02122d4dfa155b8aeda0260b06926effe3be531
SharapaGorg <62620955+SharapaGorg@users.noreply.github.com>
2020-06-15 13:56:21 +0300

    Update deps.py

--- deps.py
+++ deps.py
@@ -7,6 +7,6 @@ def run_python_tool(command):
 
 run_python_tool('pip install --upgrade pip')
 
-run_python_tool('pip install discord --user')
+run_python_tool('pip install requests discord --user')
 
 print('Bot ready to work.')
