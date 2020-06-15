dc0fa25530864545ab6111cb459066f93d728c7c
SharapaGorg <62620955+SharapaGorg@users.noreply.github.com>
2020-06-15 14:35:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,18 +17,31 @@ async def hello(ctx):
 
 @bot.command()
 async def fox(ctx):
-    response = requests.get('https://some-random-api.ml/img/fox') # Get-запрос
-    json_data = json.loads(response.text) # Извлекаем JSON
+    response = requests.get('https://some-random-api.ml/img/fox')
+    json_data = json.loads(response.text)
+    author = ctx.message.author
 
-    embed = discord.Embed(color = 0xff9900, title = 'Random Fox') # Создание Embed'a
-    embed.set_image(url = json_data['link']) # Устанавливаем картинку Embed'a
-    await ctx.send(embed = embed) # Отправляем Embed
+    embed = discord.Embed(color = 0xff9900, title = 'Random Fox')
+    embed.set_image(url = json_data['link'])
+    await ctx.send(embed = embed), print('$Bot send embed fox (by ', author.mention, ')' )
 
-print('Work is in full swing...')
+@bot.command()
+async def dog(ctx):
+    response = requests.get('https://some-random-api.ml/img/dog')
+    json_data = json.loads(response.text)
+    author = ctx.message.author
+
+    embed = discord.Embed(color = 0xff9900, title = 'Random Dog')
+    embed.set_image(url = json_data['link'])
+    await ctx.send(embed = embed), print('$Bot send embed dog (by', author.mention, ')' )
+
+
+print('\nMainThread Running')
+print('ThreadPoolExecutor-0_0 Running')
+print('Thread-6 Running\n')
+print('Work Status: Work now.\n\nSteps:\n')
 bot.run(settings['TOKEN'])
-print('Bot stoped.')
 
 #D✔Бот for discord channel
 #NzIxODQ2ODk5OTg0MDM5OTY5.Xuaeyg.08dfDqsAcWxBDv6wAfXxkXe_fCg'
 #https://discord.com/developers/applications/721846899984039969/information
-
