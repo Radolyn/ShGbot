7dfdc434ad2708181638b666d76068b17ad0c2c3
SharapaGorg <62620955+SharapaGorg@users.noreply.github.com>
2020-06-15 15:47:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,41 +6,59 @@ from config import settings
 
 bot = commands.Bot(command_prefix = settings['PREFIX'])
 
-@bot.command() 
-async def _hola_(ctx, arg):
-    await ctx.send(arg)
+try:
+    @bot.command() 
+    async def _hola_(ctx, arg):
+        await ctx.send(arg), print(f'$Bot send message: {arg}')
 
-@bot.command()
-async def hello(ctx):
-    author = ctx.message.author
-    await ctx.send(f'Hello, {author.mention}!')
+    @bot.command()
+    async def qq(ctx):
+        author = ctx.message.author
+        await ctx.send(f'Hello, {author.mention}!'), print(f'$Bot send message: Hello, {author.mention}')
 
-@bot.command()
-async def fox(ctx):
-    response = requests.get('https://some-random-api.ml/img/fox')
-    json_data = json.loads(response.text)
-    author = ctx.message.author
+    @bot.command()
+    async def bb(ctx):
+        author = ctx.message.author
+        await ctx.send(f'See you later, {author.mention} :)'), print(f'$Bot send message: Bye, {author.mention}')
 
-    embed = discord.Embed(color = 0xff9900, title = 'Random Fox')
-    embed.set_image(url = json_data['link'])
-    await ctx.send(embed = embed), print('$Bot send embed fox (by ', author.mention, ')' )
+    @bot.command()
+    async def pp(ctx):
+        author = ctx.message.author
+        await ctx.send(f'{author.mention} Отошел.'), print(f'$Bot send message: {author.mention} Отошел.')
 
-@bot.command()
-async def dog(ctx):
-    response = requests.get('https://some-random-api.ml/img/dog')
-    json_data = json.loads(response.text)
-    author = ctx.message.author
+    @bot.command()
+    async def fox(ctx):
+        response = requests.get('https://some-random-api.ml/img/fox')
+        json_data = json.loads(response.text)
+        author = ctx.message.author
 
-    embed = discord.Embed(color = 0xff9900, title = 'Random Dog')
-    embed.set_image(url = json_data['link'])
-    await ctx.send(embed = embed), print('$Bot send embed dog (by', author.mention, ')' )
+        embed = discord.Embed(color = 0xff9900, title = 'Random Fox')
+        embed.set_image(url = json_data['link'])
+        await ctx.send(embed = embed), print('$Bot send embed fox (by ', author.mention, ')' )
 
+    @bot.command()
+    async def dog(ctx):
+        response = requests.get('https://some-random-api.ml/img/dog')
+        json_data = json.loads(response.text)
+        author = ctx.message.author
 
-print('\nMainThread Running')
-print('ThreadPoolExecutor-0_0 Running')
-print('Thread-6 Running\n')
-print('Work Status: Work now.\n\nSteps:\n')
-bot.run(settings['TOKEN'])
+        embed = discord.Embed(color = 0xff9900, title = 'Random Dog')
+        embed.set_image(url = json_data['link'])
+        try: await ctx.send(embed = embed), print('$Bot send embed dog (by', author.mention, ')' )
+        except: await ctx.send('CommandNotFound', {author.mention})
+
+    print('\nMainThread Running')
+    print('ThreadPoolExecutor-0_0 Running')
+    print('Thread-6 Running\n')
+    print('Work Status: Work now.\n\nSteps:\n')
+
+    bot.run(settings['TOKEN'])
+
+except discord.ext.commands.errors.CommandNotFound: print(0)
+
+finally:
+    print('\nWell done ;)')
+    
 
 #D✔Бот for discord channel
 #NzIxODQ2ODk5OTg0MDM5OTY5.Xuaeyg.08dfDqsAcWxBDv6wAfXxkXe_fCg'
