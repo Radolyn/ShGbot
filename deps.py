cdce6f10411da1a996135a1a256ad4e8591448c1
SharapaGorg <62620955+SharapaGorg@users.noreply.github.com>
2020-06-17 16:38:19 +0300

    Update deps.py

--- deps.py
+++ deps.py
@@ -7,8 +7,9 @@ def run_python_tool(command):
 
 run_python_tool('pip install --upgrade pip')
 
-run_python_tool('pip install requests discord --user')
-
+run_python_tool('pip install requests discord youtube-dl --user')
 run_python_tool('pip install -U discord.py[voice]')
+run_python_tool('pip install --upgrade youtube-dl')
+#run_python_tool('ffmpeg -i input.mp4 output.avi')
 
-print('Bot ready to work.')
+print('Bot ready to work.')  
