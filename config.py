15ae2d9a9dae0c75cff6ccc48d9f943344784741
SharapaGorg <62620955+SharapaGorg@users.noreply.github.com>
2020-06-26 01:59:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,73 @@
+from discord.ext.commands import Bot
+
 settings = {
+    'RAID_TOKEN': 'NzIyNTgxNTMwODE5NjkwNTY2.XulO9A.NRbAok4I0UWYUynCKBiBMuOZfLE',   
     'TOKEN': 'NzIxODQ2ODk5OTg0MDM5OTY5.Xuaeyg.08dfDqsAcWxBDv6wAfXxkXe_fCg',
-    'BOT': 'D✔Бот',
+    'A_TOKEN': 'NzIyMDg4MzE1MTU5MDUyNDA4.XuqTxA.n8zhUw3GUAZlqIrO7fvaOowy1d4',
+    'BOT': 'ShGbot',
     'ID': 721846899984039969,
-    'PREFIX': '$'
+    'F_ID': 539090922449928223, 
+    'RAID_PREFIX': "--",
+    'A_PREFIX': '==',
+    'PREFIX': '$',
+    'j_TOKEN': 'NzI1MDQzOTQzNTk0NjU1OTE0.XvJAEQ.kKaSHBeJCqQhA2KgdptQ9MCpWOw',
+    'j_PREFIX': '&',
+    'Max_PREFIX': 'v!',
+    'Max_TOKEN': 'NzI1MDQ2Mjg4MTA1ODY1MjI2.XvJCGA.9kS_9vUlhE0Tob-5PT4dwBai4NE',
+    'jj_TOKEN': 'NzI1MDk1NjU2NTEwOTgwMTA2.XvJv-Q.u_TpRpSsXEDmFJyScRcqmG3HoQo',
+    'jjj_TOKEN': 'NzI1MDk1MzY2NjkyOTYyNDE1.XvJwfg.2cqsnindBaAEjRoyeeNUs5JEIco'
 }
 
-banlist = {}
+bot = Bot(settings['PREFIX'])
 
-link = {
-    '1': 'https://www.youtube.com/watch?v=zTLV6CmdK_I'
-}
+links = [
+    'https://www.youtube.com/watch?v=1ER67r8OCW8',
+    'https://i.gifer.com/7A9t.gif',
+    'https://i.gifer.com/YRwA.gif'
+]
+
+gif = [
+    'https://i.gifer.com/47tv.gif',
+    'https://i.gifer.com/GRA.gif',
+    'https://i.gifer.com/1Sqs.gif',
+    'https://i.gifer.com/4opR.gif',
+    'https://i.gifer.com/OvZ.gif',
+    'https://i.gifer.com/2ROi.gif',
+    'https://i.gifer.com/3dAZ.gif',
+    'https://i.gifer.com/2Qdl.gif',
+    'https://i.gifer.com/fxU6.gif',
+    'https://i.gifer.com/76t8.gif',
+    'https://i.gifer.com/Q81V.gif',
+    'https://i.gifer.com/2zGZ.gif',
+    'https://i.gifer.com/5m5n.gif',
+    'https://i.gifer.com/1FwN.gif',
+    'https://i.gifer.com/fyrX.gif',
+    'https://i.gifer.com/VIR.gif',
+    'https://i.gifer.com/7A9z.gif',
+    'https://i.gifer.com/2RgK.gif',
+    'https://i.gifer.com/4DPY.gif',
+    'https://i.gifer.com/ZLm5.gif',
+    'https://i.gifer.com/RyMZ.gif',
+    'https://i.gifer.com/K6Ti.gif',
+    'https://i.gifer.com/1zJg.gif',
+    'https://i.gifer.com/BPo7.gif',
+    'https://i.gifer.com/g15q.gif',
+    'https://i.gifer.com/U1nV.gif',
+    'https://i.gifer.com/4Fp0.gif',
+    'https://i.gifer.com/ynf.gif',
+    'https://i.gifer.com/cKt.gif',
+    'https://i.gifer.com/3igA.gif',
+    'https://i.gifer.com/x6.gif',
+    'https://i.gifer.com/3nxb.gif',
+    'https://i.gifer.com/7SmF.gif',
+    'https://i.gifer.com/410J.gif',
+    'https://i.gifer.com/7xFU.gif',
+    'https://i.gifer.com/iGn.gif',
+    'https://i.gifer.com/WMP.gif',
+    'https://i.gifer.com/QHC.gif',
+    'https://i.gifer.com/1Ndd.gif',      #sp
+    'https://i.gifer.com/bAV.gif',
+    'https://i.gifer.com/747m.gif'
+]
+
+#A✔A
