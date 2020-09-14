6ab22f25054a0247dad77c548381e5e7e1fd58eb
SharapaGorg <62620955+SharapaGorg@users.noreply.github.com>
2020-09-14 18:31:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1147,6 +1147,7 @@ try:
         if isinstance(error, commands.MissingPermissions):
             await ctx.send(f'{author.mention}, вы не обладаете такими правами!')
         if isinstance(error, commands.errors.CommandInvokeError):
+            LogManager.error(error)
             await ctx.send(f"```{error}```")
 
 
